69658af27ca7ece3ac2965b137f460dd316f1d9c
Elio Marcolino <eliom@jfrog.com>
2019-07-19 09:11:51 -0700

    Added GopherCon greetings response

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	"io/ioutil"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
-	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 var results []string
@@ -20,36 +22,38 @@ func main() {
 	// register hello function to handle all requests
 	server := http.NewServeMux()
 
-	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Serving request: %s", r.URL.Path)		
-	})
-
-	server.HandleFunc("/ping", hello)
+	server.HandleFunc("/ping", ping)
 	server.HandleFunc("/event", event)
-	// start the web server on port and accept requests
-	fmt.Printf("Server listening on port %s\n", port)
-
 	log.Printf("Server listening on port %s", port)
 	err := http.ListenAndServe(":"+port, server)
 	log.Fatal(err)
 }
 
 func event(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		}
-		results = append(results, string(body))
-		fmt.Fprint(w, "POST done")
-	} else if r.Method == "GET" {
-		fmt.Fprintf(w, "OK")
+	response := struct {
+		Message  string    `json:"message"`
+		Timestam time.Time `json:"timestamp"`
+	}{
+		"Welcome to GopherCon 2019",
+		time.Now(),
 	}
-	return
+
+	sendJsonResponse(w, http.StatusOK, response)
 }
 
 // ping responds to the request with a plain-text "Ok" message.
-func hello(w http.ResponseWriter, r *http.Request) {
+func ping(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
-	fmt.Fprintf(w, "Ok")
+	fmt.Fprintf(w, "Pong")
+}
+
+func sendJsonResponse(res http.ResponseWriter, httpCode int, payload interface{}) {
+	log.Infof("Sending %v response: %#v", httpCode, payload)
+	res.Header().Set("Content-Type", "application/json")
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		panic(errors.New(fmt.Sprintf("Failed to send response: %s", err)))
+	}
+	res.WriteHeader(httpCode)
+	res.Write(jsonPayload)
 }
